529bafe6a78c4c9f53b565a92b4b8d494312b9da
lc87624 <lc87624@gmail.com>
2025-02-19 21:05:24 +0800

    unicafe exercises 1.6-1.9

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>give feedback</h1>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <h1>statistics</h1>
+        {
+            good + neutral + bad === 0 ? (
+              <p>No feedback given</p>
+            ) : (
+              <p>
+                good {good} <br/>
+                neutral {neutral} <br/>
+                bad {bad} <br/>
+                all {good + neutral + bad} <br/>
+                average {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)} <br/>
+                positive {good / (good + neutral + bad) * 100} %
+              </p>
+            )
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
