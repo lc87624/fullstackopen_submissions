7efa08c0825baffe7fbbf61d1bb07b89c6a77fce
lc87624 <lc87624@gmail.com>
2026-05-11 17:05:27 +0800

    feat: enhance Blog component with conditional rendering and styling

--- bloglist_frontend/src/components/Blog.jsx
+++ bloglist_frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+  const buttonLabel = visible ? 'hide' : 'view'
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setVisible(!visible)}>{buttonLabel}</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br />
+        {blog.likes} likes
+        <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
