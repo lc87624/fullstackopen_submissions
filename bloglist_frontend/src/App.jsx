c5031f07836f03800ededeefff189302416c031a
lc87624 <lc87624@gmail.com>
2026-05-11 18:09:30 +0800

    feat: sort blogs by likes before rendering in blog list

--- bloglist_frontend/src/App.jsx
+++ bloglist_frontend/src/App.jsx
@@ -148,9 +148,11 @@ const App = () => {
 
   const blogList = () => {
     console.log('Rendering blog list with blogs:', blogs)
+    const blogsToShow = blogs.toSorted((a, b) => b.likes - a.likes)
+
     return (
       <div>
-        {blogs.map(blog =>
+        {blogsToShow.map(blog =>
           <Blog key={blog.id} blog={blog} handleClickLike={handleClickLike} />
         )}
       </div>
